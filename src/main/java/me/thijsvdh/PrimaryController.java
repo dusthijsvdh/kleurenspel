33a6feb91410fde4395ab427aea515abb8380aa7
Thijs <thijsvdh12@gmail.com>
2020-07-07 19:37:16 +0200

    added textfield
    Doesn't work though

--- src/main/java/me/thijsvdh/PrimaryController.java
+++ src/main/java/me/thijsvdh/PrimaryController.java
@@ -1,13 +1,27 @@
 package me.thijsvdh;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+import javafx.scene.input.KeyEvent;
 
 import java.io.IOException;
 
 public class PrimaryController {
+//    @FXML TextField primaryTextField;
 
     @FXML
     private void switchToSecondary() throws IOException {
         App.setRoot("secondary");
     }
+
+    // This doesn't work
+//    public void textFieldUpdate(KeyEvent keyEvent) {
+//        try {
+//            if (primaryTextField.getText() != null) {
+//                System.out.println(primaryTextField.getText());
+//            }
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        }
+//    }
 }
